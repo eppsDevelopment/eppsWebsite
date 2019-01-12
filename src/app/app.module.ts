7eb669b34c33973cb42f9bf4a67d1e5e30ab84a6
Elmer Phan <elmer.phan@gmail.com>
2019-01-12 01:18:04 -0600

    add map and forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutusComponent } from './main-nav/aboutus/aboutus.component';
 import { ContactusComponent } from './main-nav/contactus/contactus.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { MatToolbarModule, 
+  MatButtonModule, 
+  MatSidenavModule, 
+  MatIconModule, 
+  MatListModule, 
+  MatSelectModule,
+  MatInputModule,
+  MatCardModule,
+  MatRadioModule 
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
     AboutusComponent,
-    ContactusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
     MatToolbarModule,
+    MatInputModule,
     MatButtonModule,
     MatSidenavModule,
+    MatSelectModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
-    BrowserAnimationsModule
+    MatRadioModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-nav/contactus/contactus.component.ts
+++ src/app/main-nav/contactus/contactus.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpClientModule} from '@angular/common/http';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormControl, FormBuilder } from '@angular/forms'
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
 })
+
 export class ContactusComponent implements OnInit {
 
-  constructor() { }
+  public contactusForm:FormGroup;
+  public formSubmitted:boolean;
+  public defaultPhone: string = 'Mobile';
+ 
+  public phoneTypes = [
+    'Mobile',
+    'Home'
+  ];
 
-  ngOnInit() {
+//public http:HttpClient,
+  constructor( fb:FormBuilder) { 
+    this.contactusForm = fb.group({
+      'email':[],
+      'firstName':[],
+      'lastName':[],
+      'phoneNumber':[],
+      'phoneType':[],
+      // 'reason':[],
+      'description':[]
+    });
   }
 
+  ngOnInit() {
+    this.formSubmitted = false;
+  }
+
+
+  // public httpRequest():void{
+
+  //   this.http.get('httphere')
+  //   .subscribe(
+  //     data => {
+
+  //     },
+  //     error =>{
+        
+  //     }
+  //   )
+  // }
+
+  public submitClick():void{
+    console.log(this.contactusForm.value);
+
+    //this.http.post('',this.contactusForm.value)
+    // .subscribe({
+
+    // });
+    //code to send email here
+    this.formSubmitted = true;
+  }
+
+  public sendEmail():void{
+
+    
+  }
 }
